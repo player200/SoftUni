3ce4be027de728c3abe39404abc92a632a7ad57c
player200 <vasilpopo@abv.bg>
2018-02-26 22:11:20 +0200

    Interval Trees, K-d Trees - Lab done

---
+++ 11.Data Structures/Interval Trees, K-d Trees - Lab/IntervalTree/IntervalTree/IntervalTree.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class IntervalTree
+{
+    private class Node
+    {
+        internal Interval interval;
+        internal double max;
+        internal Node right;
+        internal Node left;
+
+        public Node(Interval interval)
+        {
+            this.interval = interval;
+            this.max = interval.Hi;
+        }
+    }
+
+    private Node root;
+
+    public void Insert(double lo, double hi)
+    {
+        this.root = this.Insert(this.root, lo, hi);
+    }
+
+    public void EachInOrder(Action<Interval> action)
+    {
+        EachInOrder(this.root, action);
+    }
+
+    public Interval SearchAny(double lo, double hi)
+    {
+        Node current = this.root;
+
+        while (current != null && !current.interval.Intersects(lo, hi))
+        {
+            if (current.left != null && current.left.max > lo)
+            {
+                current = current.left;
+            }
+            else
+            {
+                current = current.right;
+            }
+        }
+
+        return current?.interval;
+    }
+
+    public IEnumerable<Interval> SearchAll(double lo, double hi)
+    {
+        List<Interval> result = new List<Interval>();
+        SearchAll(this.root, lo, hi, result);
+        return result;
+    }
+
+    private void SearchAll(Node node, double lo, double hi, List<Interval> result)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        bool goLeft = node.left != null && node.left.max > lo;
+        bool goRighht = node.right != null && node.right.interval.Lo < hi;
+
+        if (goLeft)
+        {
+            SearchAll(node.left, lo, hi, result);
+        }
+
+        if (node.interval.Intersects(lo, hi))
+        {
+            result.Add(node.interval);
+        }
+
+        if (goRighht)
+        {
+            SearchAll(node.right, lo, hi, result);
+        }
+    }
+
+    private void EachInOrder(Node node, Action<Interval> action)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        EachInOrder(node.left, action);
+        action(node.interval);
+        EachInOrder(node.right, action);
+    }
+
+    private Node Insert(Node node, double lo, double hi)
+    {
+        if (node == null)
+        {
+            return new Node(new Interval(lo, hi));
+        }
+
+        int cmp = lo.CompareTo(node.interval.Lo);
+        if (cmp < 0)
+        {
+            node.left = Insert(node.left, lo, hi);
+        }
+        else if (cmp > 0)
+        {
+            node.right = Insert(node.right, lo, hi);
+        }
+
+        this.UpdateMax(node);
+        return node;
+    }
+
+    private void UpdateMax(Node node)
+    {
+        Node maxChild = GetMax(node.left, node.right);
+        node.max = GetMax(node, maxChild).max;
+    }
+
+    private Node GetMax(Node first, Node second)
+    {
+        if (first == null)
+        {
+            return second;
+        }
+
+        if (second == null)
+        {
+            return first;
+        }
+
+        return first.max > second.max ? first : second;
+    }
+}
---
+++ 11.Data Structures/Interval Trees, K-d Trees - Lab/KdTree/KdTree/KdTree.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+public class KdTree
+{
+    private const int K = 2;
+    private Node root;
+
+    public class Node
+    {
+        public Node(Point2D point)
+        {
+            this.Point = point;
+        }
+
+        public Point2D Point { get; set; }
+
+        public Node Left { get; set; }
+
+        public Node Right { get; set; }
+    }
+
+    public Node Root
+    {
+        get
+        {
+            return this.root;
+        }
+    }
+
+    public bool Contains(Point2D point)
+    {
+        Node node = this.Search(this.root, point, 0);
+        return node != null;
+    }
+
+    public void Insert(Point2D point)
+    {
+        this.root = this.Insert(this.root, point, 0);
+    }
+    
+    public void EachInOrder(Action<Point2D> action)
+    {
+        this.EachInOrder(this.root, action);
+    }
+
+    private Node Search(Node node, Point2D point, int depth)
+    {
+        if (node == null)
+        {
+            return null;
+        }
+
+        int cmp = Compare(point, node.Point, depth);
+        if (cmp < 0)
+        {
+            return Search(node.Left, point, depth + 1);
+        }
+        else if (cmp > 0)
+        {
+            return Search(node.Right, point, depth + 1);
+        }
+
+        return node;
+    }
+
+    private Node Insert(Node node, Point2D point, int depth)
+    {
+        if (node == null)
+        {
+            return new Node(point);
+        }
+
+        int compare = depth % K;
+        if (compare == 0)
+        {
+            int compareX = node.Point.X.CompareTo(point.X);
+            if (compareX > 0)
+            {
+                node.Left = this.Insert(node.Left, point, depth + 1);
+            }
+            else if (compareX <= 0)
+            {
+                node.Right = this.Insert(node.Right, point, depth + 1);
+            }
+        }
+        else
+        {
+            int compareY = node.Point.Y.CompareTo(point.Y);
+            if (compareY > 0)
+            {
+                node.Left = this.Insert(node.Left, point, depth + 1);
+            }
+            else if (compareY <= 0)
+            {
+                node.Right = this.Insert(node.Right, point, depth + 1);
+            }
+        }
+
+        return node;
+    }
+
+    private void EachInOrder(Node node, Action<Point2D> action)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        this.EachInOrder(node.Left, action);
+        action(node.Point);
+        this.EachInOrder(node.Right, action);
+    }
+
+    private int Compare(Point2D first, Point2D second, int depth)
+    {
+        int cmp = 0;
+        if (depth % K == 0)
+        {
+            cmp = first.X.CompareTo(second.X);
+            if (cmp == 0)
+            {
+                cmp = first.Y.CompareTo(second.Y);
+            }
+
+            return cmp;
+        }
+        else
+        {
+            cmp = first.Y.CompareTo(second.Y);
+            if (cmp == 0)
+            {
+                cmp = first.X.CompareTo(second.X);
+            }
+        }
+
+        return cmp;
+    }
+}
