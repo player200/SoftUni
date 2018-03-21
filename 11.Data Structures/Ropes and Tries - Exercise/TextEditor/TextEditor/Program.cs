20608e5b6cbaefe43961fd2a5ccf8011cdc3ed88
player200 <vasilpopo@abv.bg>
2018-03-21 14:09:11 +0200

    Add exercise

---
+++ 11.Data Structures/Ropes and Tries - Exercise/TextEditor/TextEditor/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        var input = Console.ReadLine();
+        var textEditor = new TextEditor();
+
+        string user = "";
+        string command = "";
+        string pattern = @"""(.*)""";
+
+        while (input.ToLower() != "end")
+        {
+            var text = Regex.Match(input, pattern).Groups[1].Value;
+            var parameters = input.Split();
+
+            if (!textEditor.UserExists(user) && parameters[0] != "login")
+            {
+                input = Console.ReadLine();
+                continue;
+            }
+
+            if (parameters[0] == "login")
+            {
+                command = parameters[0];
+                user = parameters[1];
+            }
+            else if (parameters[0] == "users")
+            {
+                command = parameters[0];
+            }
+            else
+            {
+                command = parameters[1];
+                user = parameters[0];
+            }
+
+            switch (command)
+            {
+                case "login":
+                    textEditor.Login(user);
+                    break;
+                case "insert":
+                    textEditor.Insert(user, int.Parse(parameters[2]), text);
+                    break;
+                case "prepend":
+                    textEditor.Prepend(user, text);
+                    break;
+                case "print":
+                    Console.WriteLine(textEditor.Print(user));
+                    break;
+                case "length":
+                    Console.WriteLine(textEditor.Length(user));
+                    break;
+                case "delete":
+                    textEditor.Delete(user, int.Parse(parameters[2]), int.Parse(parameters[3]));
+                    break;
+                case "substring":
+                    textEditor.Substring(user, int.Parse(parameters[2]), int.Parse(parameters[3]));
+                    break;
+                case "users":
+                    Console.WriteLine(string.Join(Environment.NewLine, textEditor.Users(parameters.Length > 1 ? parameters[1] : string.Empty)));
+                    break;
+                case "undo":
+                    textEditor.Undo(user);
+                    break;
+                case "clear":
+                    textEditor.Clear(user);
+                    break;
+                case "logout":
+                    textEditor.Logout(user);
+                    break;
+            }
+
+            input = Console.ReadLine();
+        }
+    }
+}
---
+++ 11.Data Structures/Ropes and Tries - Exercise/TextEditor/TextEditor/TextEditor.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Wintellect.PowerCollections;
+
+public class TextEditor : ITextEditor
+{
+    private IDictionary<string, StringBuilder> users;
+
+    private IDictionary<string, Stack<string>> texts;
+
+    public TextEditor()
+    {
+        this.users = new Dictionary<string, StringBuilder>();
+        this.texts = new Dictionary<string, Stack<string>>();
+    }
+
+    public void Clear(string username)
+    {
+        this.Cache(username);
+        this.users[username].Clear();
+    }
+
+    public void Delete(string username, int startIndex, int length)
+    {
+        this.Cache(username);
+        this.users[username].Remove(startIndex, length);
+    }
+
+    public void Insert(string username, int index, string str)
+    {
+        this.Cache(username);
+        this.users[username].Insert(index, str);
+    }
+
+    public int Length(string username) => this.users[username].Length;
+
+    public void Login(string username)
+    {
+        if (!this.users.ContainsKey(username))
+        {
+            this.users.Add(username, new StringBuilder());
+            this.texts.Add(username, new Stack<string>());
+        }
+    }
+
+    public void Logout(string username)
+    {
+        this.users.Remove(username);
+        this.texts.Remove(username);
+    }
+
+    public void Prepend(string username, string str)
+    {
+        if (this.users.ContainsKey(username))
+        {
+            this.Cache(username);
+            this.users[username].Insert(0, str);
+        }
+    }
+
+    public string Print(string username) => this.users[username].ToString();
+
+    public void Substring(string username, int startIndex, int length)
+    {
+        this.Cache(username);
+        this.users[username] = new StringBuilder(this.users[username].ToString().Substring(startIndex, length));
+    }
+
+    public void Undo(string username)
+    {
+        if (this.texts[username].Count != 0)
+        {
+            this.users[username] = new StringBuilder(this.texts[username].Pop());
+        }
+    }
+
+    public IEnumerable<string> Users(string prefix = "")
+    {
+        if (string.IsNullOrEmpty(prefix))
+        {
+            return this.users.Keys;
+        }
+
+        return this.users.Keys.Where(u => u.StartsWith(prefix));
+    }
+
+    public bool UserExists(string username) => this.users.ContainsKey(username);
+
+    private void Cache(string username)
+    {
+        this.texts[username].Push(this.users[username].ToString());
+    }
+}
